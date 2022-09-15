182aa9ad1aa262ec0a7eea915fb7ebe925a6100d
sumaya-kabir <sumayakabir4547@gmail.com>
2022-09-15 10:55:16 +0600

    mistakes count solved

--- script.js
+++ script.js
@@ -46,6 +46,7 @@ const typeController = (e) => {
     display.innerHTML += `<span class="green">${newLetter === " " ? "▪" : newLetter}</span>`;
   } else {
     display.innerHTML += `<span class="red">${newLetter === " " ? "▪" : newLetter}</span>`;
+    errorCount++;
   }
 
   // check if given question text is equal to user typed text
@@ -68,6 +69,7 @@ const gameOver = () => {
   // so total time taken is current time - start time
   const finishTime = new Date().getTime();
   const timeTaken = (finishTime - startTime) / 1000;
+  
 
   // show result modal
   resultModal.innerHTML = "";
